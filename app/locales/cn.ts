b30e548eab824a676511513516f5d08625501a26
yidian <yidianyu322@gmail.com>
2024-03-01 10:30:49 +0800

    adf

--- app/locales/cn.ts
+++ app/locales/cn.ts
@@ -161,12 +161,12 @@ const cn = {
     },
 
     Update: {
-      // Version: (x: string) => `当前版本：${x}`,
-      // IsLatest: "已是最新版本",
-      // CheckUpdate: "检查更新",
-      // IsChecking: "正在检查更新...",
-      // FoundUpdate: (x: string) => `发现新版本：${x}`,
-      // GoToUpdate: "",
+      Version: (x: string) => `当前版本：${x}`,
+      IsLatest: "已是最新版本",
+      CheckUpdate: "检查更新",
+      IsChecking: "正在检查更新...",
+      FoundUpdate: (x: string) => `发现新版本：${x}`,
+      GoToUpdate: "",
     },
     SendKey: "发送键",
     Theme: "主题",
